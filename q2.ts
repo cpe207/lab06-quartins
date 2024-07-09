a6da85d7f65b7999f719a0958132e007a0842295
nani <chotima.nani@gmail.com>
2024-07-09 09:41:19 +0700

    add

--- q2.ts
+++ q2.ts
@@ -3,9 +3,24 @@ import axios from "axios";
 
 /* assign interface/type to the function definition properly */
 const getTodo = async (todoId: number) => {
-  /* Your code here */
-};
+    try{
+      const res = await axios.get(`https://jsonplaceholder.typicode.com/todos/${todoId}`);
+      const todo = res.data;
+      
+      const userId = todo.userId;
+      const userres = await axios.get(`https://jsonplaceholder.typicode.com/users/${userId}`);
+      const userdata = userres.data;
 
+      const result = {
+        owner : userdata.name,
+        title: todo.title,
+        completed: todo.completed,
+      };
+      return result;
+    } catch(error){
+      return 'INVALID TODO ID';
+    }
+}
 //test case
 const input1 = 15;
 const input2 = 60;
